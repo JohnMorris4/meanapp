b3527943f2a98de0cb70a78f6f35f724f03dfe7e
JohnMorris4 <johnmorris@morrisje.com>
2017-08-07 10:12:19 -0400

    Pushing again after reset

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
+  database: 'mongodb://john:john@ds015889.mlab.com:15889/meanauthapp',
+  secret: 'yoursecret'
+}
